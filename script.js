6a1cd32bc6f92b0a3bcb626f1633d9ff71853a17
Your Name <booojglhj@gmail.com>
2021-09-13 16:23:54 +0400

    Hello Wordl!

---
+++ script.js
@@ -0,0 +1,13 @@
+// let icons = document.querySelectorAll(".icons-all");
+
+// class Block {
+//   constructor() {
+
+//   }
+//   names() {
+//     icons.forEach((item) => {
+//       item.style.boxShadow = "2px 10px 28px -5px #fa4a0c";
+//     });
+//   }
+//   names();
+// }
